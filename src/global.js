818c0766bb221a3895fc9235cf5e35228d386981
AdjWang <wwang230513@gmail.com>
2026-08-16 19:05:05 +0800

    Add unicorn basic input control.

--- src/global.js
+++ src/global.js
@@ -4,3 +4,9 @@ const TILE_SIZE = 16;  // pixels
 
 const WORLD_WIDTH = 30;  // cells
 const WORLD_HEIGHT = 20;  // cells
+
+const INPUT_KEY_UP = 'ArrowUp';
+const INPUT_KEY_DOWN = 'ArrowDown';
+const INPUT_KEY_LEFT = 'ArrowLeft';
+const INPUT_KEY_RIGHT = 'ArrowRight';
+const INPUT_KEY_JUMP = 'Space';
--- src/unicorn.js
+++ src/unicorn.js
@@ -2,19 +2,22 @@
 
 const UNICORN_WIDTH = 21;  // pixels
 const UNICORN_HEIGHT = 24;  // pixels
+const UNICORN_RUN_SPEED = .08;
+const UNICORN_JUMP_INITIAL_SPEED = .22;
 
 class Unicorn extends EngineObject {
   #ANIM_STATE_IDLE = 0;
   #ANIM_STATE_RUN = 1;
 
   #FRAME_INDEX_IDLE = 0;
+  #FRAME_INDEX_JUMP = 0;
   #FRAME_INDEX_RUN = 1;
   #FRAME_COUNT_RUN = 2;
   #ANIM_RUN_SPEED = 8;  // frame/sec
 
-  #playerHead;
-  #playerBody;
-  #playerBag;
+  #frameInfoHead;
+  #frameInfoBody;
+  #frameInfoBag;
   #animState = this.#ANIM_STATE_IDLE;
   #runFrame = this.#FRAME_INDEX_IDLE;
   #runFrameTimer = new Timer(1.0 / this.#ANIM_RUN_SPEED);
@@ -22,16 +25,33 @@ class Unicorn extends EngineObject {
   constructor(pos) {
     const size = vec2(UNICORN_WIDTH / TILE_SIZE, UNICORN_HEIGHT / TILE_SIZE);
     super(pos, size);
-    this.#playerHead = tile(0, vec2(UNICORN_WIDTH, UNICORN_HEIGHT), 1);
-    this.#playerBody = tile(0, vec2(UNICORN_WIDTH, UNICORN_HEIGHT), 2);
-    this.#playerBag =  tile(0, vec2(UNICORN_WIDTH, UNICORN_HEIGHT), 3);
+    this.#frameInfoHead = tile(0, vec2(UNICORN_WIDTH, UNICORN_HEIGHT), 1);
+    this.#frameInfoBody = tile(0, vec2(UNICORN_WIDTH, UNICORN_HEIGHT), 2);
+    this.#frameInfoBag =  tile(0, vec2(UNICORN_WIDTH, UNICORN_HEIGHT), 3);
     this.setCollision();
   }
 
   update() {
+    const moveX = keyDirection(INPUT_KEY_UP, INPUT_KEY_DOWN, INPUT_KEY_LEFT, INPUT_KEY_RIGHT).x;
+    const jumpPressed = keyWasPressed(INPUT_KEY_UP) || keyWasPressed(INPUT_KEY_JUMP);
+    const grounded = !!this.groundObject;
+
+    this.velocity.x = moveX * UNICORN_RUN_SPEED;
+    if (jumpPressed && grounded) {
+      this.velocity.y = UNICORN_JUMP_INITIAL_SPEED;
+    }
+    if (moveX) {
+      this.mirror = moveX > 0;
+    }
+
     super.update();
 
-    this.#animState = abs(this.velocity.x) > .01 ? this.#ANIM_STATE_RUN : this.#ANIM_STATE_IDLE;
+    if (!this.groundObject) {
+      this.#runFrame = this.#FRAME_INDEX_JUMP;
+      return;
+    }
+
+    this.#animState = moveX ? this.#ANIM_STATE_RUN : this.#ANIM_STATE_IDLE;
     if (this.#animState == this.#ANIM_STATE_IDLE) {
       this.#runFrame = this.#FRAME_INDEX_IDLE;
       return;
@@ -48,8 +68,8 @@ class Unicorn extends EngineObject {
   }
 
   render() {
-    drawTile(this.pos, this.size, this.#playerHead);
-    drawTile(this.pos, this.size, this.#playerBody.frame(this.#runFrame));
-    drawTile(this.pos, this.size, this.#playerBag);
+    drawTile(this.pos, this.size, this.#frameInfoHead, undefined, 0, this.mirror);
+    drawTile(this.pos, this.size, this.#frameInfoBody.frame(this.#runFrame), undefined, 0, this.mirror);
+    drawTile(this.pos, this.size, this.#frameInfoBag, undefined, 0, this.mirror);
   }
 }
