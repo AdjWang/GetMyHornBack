fc91324c8d363210658a3f07829e29b6c531f964
AdjWang <wwang230513@gmail.com>
2026-08-21 23:43:40 +0800

    Cleanup.

--- src/main.js
+++ src/main.js
@@ -8,75 +8,20 @@
 'use strict';
 
 // game variables
-let foreground;
-let background;
 let backgroundMusic;
 let backgroundMusicVolume = 0.0;
-let tileLayer;
 
 // WebGL can be removed to save ~963 bytes - see "Disabling WebGL" in README.md
 
-const level1 = [
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,1,2,2,2,2,2,2,2,2,2,2,2,3,0,0,0,0,0,0,0,0,0,0,
-0,6,7,7,7,7,7,7,7,7,7,7,7,8,0,0,0,0,0,0,0,0,0,0,
-0,6,7,7,7,7,7,7,7,7,7,7,7,8,0,0,0,0,0,0,0,0,0,0,
-0,6,7,7,7,7,7,7,7,7,7,7,7,8,0,0,0,0,0,0,0,0,0,0,
-0,11,12,12,12,12,12,12,12,12,12,12,12,13,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15
-];
-
 ///////////////////////////////////////////////////////////////////////////////
-let player;
-let dragon;
 let rainbowCellPreview;
 let rainbowCellMousePreview;
 
 async function gameInit() {
-    // Remap scene theme.
-    const sceneTheme = THEME_INDEX_CLOUD;
-    await remapTilesetColor(0, sceneTheme);
-    foreground = new Foreground(sceneTheme);
-    background = new Background(sceneTheme);
-
-    // Create tile collision and visible tile layer.
-    initTileCollision(vec2(WORLD_WIDTH, WORLD_HEIGHT));
-    const pos = vec2(0, 0);
-    tileLayer = new TileLayer(pos, tileCollisionSize);
-    tileLayer.renderOrder = RENDER_ORDER_TILE_LAYER;
-    for (let y = 0; y < tileCollisionSize.y; y++) {
-        for (let x = 0; x < tileCollisionSize.x; x++) {
-            let pos = vec2(x, tileCollisionSize.y - y - 1);
-            // set tile data
-            const tile_id = level1[y * WORLD_WIDTH + pos.x];
-            if (tile_id == 0) {
-                continue;
-            }
-            const tileIndex = tile_id - 1;
-            const data = new TileLayerData(tileIndex);
-            tileLayer.setData(pos, data);
-            setTileCollisionData(pos, 1);
-        }
-    }
-    // Draw tile layer with new data.
-    tileLayer.tileInfo = tile(0, 16, TEXTURE_INDEX_TILESET, 0);
-    tileLayer.redraw();
-    // Setup camera.
+    await loadLevel(0);
     updateWorldCamera();
-    // Enable gravity.
     gravity.y = -0.01;
 
-    player = new Unicorn(vec2(10, 18));
-    dragon = new Dragon(vec2(5, 18));
     rainbowCellMousePreview = new RainbowCellMousePreview;
     rainbowCellPreview = new RainbowCellPreview;
 
