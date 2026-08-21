37c14080dfcb39812824da2806101f640868cc4f
AdjWang <wwang230513@gmail.com>
2026-08-21 22:28:45 +0800

    Add rainbow cell border.

--- src/rainbowCell.js
+++ src/rainbowCell.js
@@ -2,12 +2,15 @@
 
 const RAINBOW_CELL_TIME = 1.0;
 const RAINBOW_CELL_RENDER_ORDER = RENDER_ORDER_BULLET - 0.5;
-const RAINBOW_CELL_PREVIEW_COLOR = new Color(1, 1, 1, 0.15);
+const RAINBOW_CELL_PREVIEW_DASH_COUNT = 6;
+const RAINBOW_CELL_PREVIEW_DASH_THICKNESS = 0.1;
+const RAINBOW_CELL_PREVIEW_DARK_COLOR = new Color(0, 0, 0, 0.9);
+const RAINBOW_CELL_PREVIEW_LIGHT_COLOR = new Color(1, 1, 1, 0.9);
 const RAINBOW_CELL_ACTIVE_COLOR = new Color(1, 1, 1, 1);
 
 class RainbowCell extends EngineObject {
   constructor() {
-    super(vec2(), vec2(1), undefined, 0, RAINBOW_CELL_PREVIEW_COLOR, RAINBOW_CELL_RENDER_ORDER);
+    super(vec2(), vec2(1), undefined, 0, new Color, RAINBOW_CELL_RENDER_ORDER);
     this.mass = 0;
     this.setCollision(true, true, false, true);
   }
@@ -17,7 +20,7 @@ class RainbowCell extends EngineObject {
   }
 
   render() {
-    drawRect(this.pos, this.size, RAINBOW_CELL_PREVIEW_COLOR);
+    drawRainbowCellPreview(this.pos, this.size);
   }
 
   createStandableCell() {
@@ -26,6 +29,26 @@ class RainbowCell extends EngineObject {
 
   collideWithObject(o) {
     return false;
+  }
+}
+
+function drawRainbowCellPreview(pos, size) {
+  const minX = pos.x - size.x / 2;
+  const maxX = pos.x + size.x / 2;
+  const minY = pos.y - size.y / 2;
+  const maxY = pos.y + size.y / 2;
+  drawRainbowCellPreviewSide(vec2(minX, minY), vec2(maxX, minY));
+  drawRainbowCellPreviewSide(vec2(maxX, minY), vec2(maxX, maxY));
+  drawRainbowCellPreviewSide(vec2(maxX, maxY), vec2(minX, maxY));
+  drawRainbowCellPreviewSide(vec2(minX, maxY), vec2(minX, minY));
+}
+
+function drawRainbowCellPreviewSide(start, end) {
+  for (let i = 0; i < RAINBOW_CELL_PREVIEW_DASH_COUNT; ++i) {
+    const dashStart = start.lerp(end, i / RAINBOW_CELL_PREVIEW_DASH_COUNT);
+    const dashEnd = start.lerp(end, (i + 0.5) / RAINBOW_CELL_PREVIEW_DASH_COUNT);
+    const color = i % 2 ? RAINBOW_CELL_PREVIEW_DARK_COLOR : RAINBOW_CELL_PREVIEW_LIGHT_COLOR;
+    drawLine(dashStart, dashEnd, RAINBOW_CELL_PREVIEW_DASH_THICKNESS, color);
   }
 }
 
